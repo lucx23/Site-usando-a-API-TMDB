208fc9f7e8028c9352816dae31946571419ac1c8
lucx23 <lucas.galdino.714@ufrn.edu.br>
2026-04-30 17:05:17 -0300

    configurando app.js

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const rotaFilmes = require('./routes/rota_filmes');
 const rotaUsuarios = require('./routes/rota_usuarios');
 const expressLayouts = require('express-ejs-layouts');
 const expressSession = require('express-session');
+const mysql = require('mysql2');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+const connection = mysql.createConnection({
+    uri: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Erro ao conectar ao banco de dados:', err);
+    } else {
+        console.log('Conectado ao banco de dados MySQL');
+    }
+});
 
 app.use(expressSession({
     secret: process.env.SECRET_SITE,
@@ -15,8 +31,11 @@ app.use(expressSession({
     saveUninitialized: false,
     cookie: {
         maxAge: 1000 * 60 * 60 * 24, // 1 dia
+        secure: true,
     }
 }))
+
+
 
 
 app.use((req, res, next) => {
